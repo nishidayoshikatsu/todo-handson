a178d2b40a66318bc16182885df498e37503c796
Yoshikatsu Nishida <nsd24technology@gmail.com>
2021-07-17 11:30:32 +0900

    Day2のソースを追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log("test");
+
+// index.htmlのaddListButtonクラスを取得
+const addTask = document.getElementsByClassName("addListButton")[0];
+
+// TODO: 削除ボタンを追加してみよう
+addTask.addEventListener("click", (event) => {
+  const task = "タスク追加";
+
+  const viewTask = document.getElementsByClassName("viewTask")[0];
+  const listItem = document.createElement("li"); // liタグを生成
+  listItem.innerText = task; // listItemにtaskという変数の中身を入力
+  viewTask.appendChild(listItem); // viewTaskの要素の中身の最後にlistItemを追加
+});
