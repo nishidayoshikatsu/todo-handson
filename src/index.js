ca748c4849737c5d56d30bc9599fbf5e636fe30d
Yoshikatsu Nishida <nsd24technology@gmail.com>
2021-07-18 11:57:37 +0900

    追加

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ console.log("test");
 
 // index.htmlのaddListButtonクラスを取得
 const addTask = document.getElementsByClassName("addListButton")[0];
+const content = document.getElementsByClassName("inputContent")[0];
 
 // TODO: 削除ボタンを追加してみよう
 addTask.addEventListener("click", (event) => {
-  const task = "タスク追加";
+  const task = content.value;
 
   const viewTask = document.getElementsByClassName("viewTask")[0];
   const listItem = document.createElement("li"); // liタグを生成
+
+  const removeButton = document.createElement("button"); // 削除ボタンを追加
+  removeButton.innerText = "削除";
+  removeButton.addEventListener("click", () => {
+    const targetTask = removeButton.closest("li");
+    viewTask.removeChild(targetTask);
+  });
+
   listItem.innerText = task; // listItemにtaskという変数の中身を入力
+  listItem.append(removeButton);
+
   viewTask.appendChild(listItem); // viewTaskの要素の中身の最後にlistItemを追加
+  // 入力後、入力フォームの値を削除する
+  content.value = "";
 });
+
+// TODO: 関数を使ってコードを読みやすくしよう
